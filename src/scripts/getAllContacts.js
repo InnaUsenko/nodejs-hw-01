d7d84af13bad55417d30d91cc89b6102ae234424
InnaUsenko <innausenko8@gmail.com>
2024-11-03 01:49:29 +0200

    getAllContacts readAllContacts

--- src/scripts/getAllContacts.js
+++ src/scripts/getAllContacts.js
@@ -1,3 +1,14 @@
-export const getAllContacts = async () => {};
+import { PATH_DB } from '../constants/contacts.js';
+import fs from 'node:fs/promises';
+
+export const getAllContacts = async () => {
+  try {
+    const dataContacts = await fs.readFile(PATH_DB);
+    return JSON.parse(dataContacts);
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+};
 
 console.log(await getAllContacts());
--- src/utils/readContacts.js
+++ src/utils/readContacts.js
@@ -1,3 +1,12 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'node:fs/promises';
 
-export const readContacts = async () => {};
+export const readContacts = async () => {
+  try {
+    const dataContacts = await fs.readFile(PATH_DB);
+    return JSON.parse(dataContacts);
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+};
